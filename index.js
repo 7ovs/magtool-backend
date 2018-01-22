5d3502d702a5b278d858927bf5042f6605ad8cad
rpm42 <rpm42@yandex.com>
2018-01-22 13:49:44 +0300

    add bodyParser

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 var express = require('express')
 var cors = require('cors')
+var bodyParser = require('body-parser')
+
 var app = express()
+
+var jsonParser = bodyParser.json()
 
 app.use(cors())
 
