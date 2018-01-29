0f68b5da61d13c15991385d6b07c9355d9fc1105
rpm42 <rpm42@yandex.com>
2018-01-29 14:59:14 +0300

    add create-user binary

---
+++ bin/create-user.js
@@ -0,0 +1,97 @@
+var RedisStore = require('../lib/store')
+var config = require('../etc/config.json')
+var hash = require('pbkdf2-password')()
+
+var readInput = async () => {
+  var question = async (query, password = false) => {
+    return new Promise((resolve, reject) => {
+      var result = ''
+      process.stdin.setEncoding('utf8')
+      process.stdin.setRawMode(true)
+      process.stdout.write(query)
+      const onReadable = () => {
+        const chunk = process.stdin.read()
+        if (chunk !== null) {
+          if (chunk === '\n' || chunk === '\r' || chunk === '\u0004') {
+            process.stdout.write('\n')
+            process.stdin.setRawMode(false)
+            process.stdin.removeListener('readable', onReadable)
+            resolve(result)
+          } else if (chunk === '\u0003') {
+            process.stdout.write('\n')
+            process.stdin.setRawMode(false)
+            process.stdin.removeListener('readable', onReadable)
+            reject(new Error('Input interrupted'))
+          } else {
+            result += chunk
+            if (password) {
+              process.stdout.write('*')
+            } else {
+              process.stdout.write(chunk)
+            }
+          }
+        }
+      }
+      process.stdin.on('readable', onReadable)
+    })
+  }
+
+  var result = {}
+
+  try {
+    result.username = await question('username : ')
+    result.password = await question('password : ', true)
+  } catch (error) {
+    process.stdin.emit('end')
+    throw error
+  }
+
+  process.stdin.emit('end')
+
+  return result
+}
+
+var createHash = async (password) => {
+  return new Promise((resolve, reject) => {
+    hash({ password }, (err, pass, salt, hash) => {
+      if (err) throw reject(err)
+      resolve({ salt, hash })
+    })
+  })
+}
+
+var timeout = async (timeoutValue) => {
+  return new Promise((resolve) => {
+    setTimeout(resolve, timeoutValue)
+  })
+}
+
+var main = async () => {
+  if (!require('fs').existsSync(require('path').resolve('../' + config.redis.pidfile))) {
+    await RedisStore.startSrever(config.redis)
+    await timeout(500)
+  } else {
+    console.log('server already started')
+  }
+
+  var store = new RedisStore(config.redis)
+
+  try {
+    const { username, password } = await readInput()
+
+    const userobj = {
+      name: username,
+      ...await createHash(password)
+    }
+
+    await store.setMapVal('users', username, userobj)
+
+    console.log('4. saved', userobj)
+  } catch (error) {
+    console.error(error.message)
+  }
+
+  RedisStore.stopServer()
+}
+
+main()
