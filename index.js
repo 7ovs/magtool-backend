de983dc6df7b22285ef11c60b11111b90c237dda
rpm42 <rpm42@yandex.com>
2018-01-29 16:44:16 +0300

    replace express-session on JWT-tokens

--- index.js
+++ index.js
@@ -1,102 +1,105 @@
 var express = require('express')
 var cors = require('cors')
 var bodyParser = require('body-parser')
-var session = require('express-session')
 var hash = require('pbkdf2-password')()
 var config = require('./etc/config.json')
-var store = require('./lib/store') // eslint-disable-line
+var jwt = require('jsonwebtoken')
+var RedisStore = require('./lib/store')
+var { timeout } = require('./lib/util')
 
 var app = express()
 
-app.use(session({
-  resave: false,
-  saveUninitialized: false,
-  secret: 'shhhh, very secret'
-}))
-
 var jsonParser = bodyParser.json()
 
-var users = {
-  admin: {
-    name: 'admin'
-  }
-}
-
-hash({ password: 'secret' }, (err, pass, salt, hash) => {
-  if (err) throw err
-  users.admin.salt = salt
-  users.admin.hash = hash
-})
-
-var auth = (name, password, callback) => {
-  const user = users[name]
-  if (!user) return callback(new Error('cannot find user'))
-  hash({ password, salt: user.salt }, (err, pass, salt, hash) => {
-    if (err) return callback(err)
-    if (hash === user.hash) return callback(null, user)
-    callback(new Error('invalid password'))
-  })
-}
-
-var checkAuth = (req, res, next) => {
-  if (req.session.user) {
-    next()
+var main = async () => {
+  if (!require('fs').existsSync(require('path').resolve('../' + config.redis.pidfile))) {
+    await RedisStore.startSrever(config.redis)
+    await timeout(500)
   } else {
-    res.json({
-      status: 'FAIL',
-      error: 'login is required'
+    console.log('server already started')
+  }
+
+  var store = new RedisStore(config.redis)
+  var users = await store.getMap('users')
+
+  console.log(users)
+
+  var auth = (name, password, callback) => {
+    const user = users[name]
+    if (!user) return callback(new Error('cannot find user'))
+    hash({ password, salt: user.salt }, (err, pass, salt, hash) => {
+      if (err) return callback(err)
+      if (hash === user.hash) return callback(null, user)
+      callback(new Error('invalid password'))
     })
   }
-}
 
-app.use(cors())
+  // TODO
+  var checkAuth = (req, res, next) => {
+    try {
+      const token = req.headers['x-access-token']
+      if (!token) throw (new Error('login is required'))
 
-app.get('/', (req, res) => {
-  res.send('magtool v0.1.0')
-})
+      const session = jwt.verify(token, config.jwt_secret)
 
-app.post('/login', jsonParser, (req, res) => {
-  if (!req.body) return res.sendStatus(400)
-  auth(req.body.username, req.body.password, (err, user) => {
-    if (err || !user) {
+      if (!users[session.username]) throw (new Error('user not found'))
+      next()
+    } catch (error) {
       res.json({
         status: 'FAIL',
-        error: 'invalid username or password'
+        error: error.message
       })
-      return
     }
+  }
 
-    req.session.regenerate(() => {
-      req.session.user = user
+  app.use(cors())
+
+  app.get('/', (req, res) => {
+    res.send('magtool v0.1.0')
+  })
+
+  app.post('/login', jsonParser, (req, res) => {
+    if (!req.body) return res.sendStatus(400)
+    auth(req.body.username, req.body.password, (err, user) => {
+      if (err || !user) {
+        res.json({
+          status: 'FAIL',
+          error: 'invalid username or password'
+        })
+        return
+      }
+
+      var token = jwt.sign({ username: user.name }, config.jwt_secret)
       res.json({
-        status: 'OK'
+        status: 'OK',
+        token
       })
     })
   })
-})
 
-app.post('/logout', jsonParser, (req, res) => {
-  req.session.destroy(() => {
+  app.post('/logout', jsonParser, (req, res) => {
     res.json({
       status: 'OK'
     })
   })
-})
 
-app.post('/command', checkAuth, jsonParser, (req, res) => {
-  if (!req.body) return res.sendStatus(400)
-  const cmd = req.body.command
-  switch (cmd) {
-    case 'PING':
-      res.json({
-        status: 'OK',
-        data: 'PONG'
-      })
-      break
-    default:
-      break
-  }
-})
+  app.post('/command', checkAuth, jsonParser, (req, res) => {
+    if (!req.body) return res.sendStatus(400)
+    const cmd = req.body.command
+    switch (cmd) {
+      case 'PING':
+        res.json({
+          status: 'OK',
+          data: 'PONG'
+        })
+        break
+      default:
+        break
+    }
+  })
 
-app.listen(config.port)
-console.log(`start server on port ${config.port}`)
+  app.listen(config.port)
+  console.log(`start server on port ${config.port}`)
+}
+
+main()
